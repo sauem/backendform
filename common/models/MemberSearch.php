f983b66b92bd5278792405d1ffbf811b7dfd79d8
sauem <nguyendinhthang.go97@gmail.com>
2021-04-29 14:19:48 +0700

    update module

---
+++ common/models/MemberSearch.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace common\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Member;
+
+/**
+ * MemberSearch represents the model behind the search form of `common\models\Member`.
+ */
+class MemberSearch extends Member
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'media_id', 'created_at', 'updated_at'], 'integer'],
+            [['name', 'description', 'facebook', 'twitter', 'instagram', 'youtube', 'linkedin'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Member::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'media_id' => $this->media_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'facebook', $this->facebook])
+            ->andFilterWhere(['like', 'twitter', $this->twitter])
+            ->andFilterWhere(['like', 'instagram', $this->instagram])
+            ->andFilterWhere(['like', 'youtube', $this->youtube])
+            ->andFilterWhere(['like', 'linkedin', $this->linkedin]);
+
+        return $dataProvider;
+    }
+}
