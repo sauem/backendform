17f01de59d1a843137bff78b10710ab57f7ce887
sauem <nguyendinhthang.go97@gmail.com>
2021-05-14 09:24:18 +0700

    put

--- console/migrations/m210429_035433_memmber_table.php
+++ console/migrations/m210429_035433_memmber_table.php
@@ -17,7 +17,7 @@ class m210429_035433_memmber_table extends Migration
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
-
+        //HELLO
         $this->createTable('member', [
             'id' => $this->primaryKey(),
             'name' => $this->string(255),
