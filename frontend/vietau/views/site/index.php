0a3bea9b33a3715c90c4524f3b6aad3890a41704
sauem <nguyendinhthang.go97@gmail.com>
2021-05-19 10:10:45 +0700

    lag

--- frontend/vietau/views/site/index.php
+++ frontend/vietau/views/site/index.php
@@ -149,19 +149,6 @@ Yii::$app->params['footer_type'] = 'light';
                         <h2><?= Yii::t('app', 'schedule') ?></h2>
                     </div>
                     <div class="blocks-outer">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                            has
-                            been the industry's standard dummy text ever since the 1500s, when an unknown printer
-                            took a
-                            galley of type and scrambled it to make a type specimen book. It has survived not only
-                            five
-                            centuries, but also the leap into electronic typesetting, remaining essentially
-                            unchanged.
-                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem
-                            Ipsum
-                            passages, and more recently with desktop publishing software like Aldus PageMaker
-                            including
-                            versions of Lorem Ipsum</p>
                         <!-- Reputation Block -->
                         <?= HelperFunction::setting('about_us', true) ?>
 
