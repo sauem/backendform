47bb75291ebab9a170d98d13adba93a99bdd415b
sauem <nguyendinhthang.go97@gmail.com>
2021-05-21 17:11:48 +0700

    lag

--- frontend/vietau/views/parts/page-title.php
+++ frontend/vietau/views/parts/page-title.php
@@ -4,5 +4,6 @@
             <li><a href="/"><?= Yii::t('app', 'home') ?></a></li>
             <li><?= $title ? $title : 'Archive' ?></li>
         </ul>
+        <p><?= isset($description) ? $description : '' ?></p>
     </div>
 </div>
