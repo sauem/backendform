a2287ae3305e8123feaa47b23987ce3c20952f11
sauem <nguyendinhthang.go97@gmail.com>
2021-05-21 12:09:19 +0700

    lag

--- common/config/params.php
+++ common/config/params.php
@@ -1,4 +1,5 @@
 <?php
+$local = require __DIR__ . '/params-local.php';
 return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
@@ -6,21 +7,5 @@ return [
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
-    'db' => [
-        'vietaulocal' => [
-            'database' => 'vietau',
-            'username' => 'sauem',
-            'password' => '123',
-        ],
-        'usvnlocal' => [
-            'database' => 'usvn',
-            'username' => 'sauem',
-            'password' => '123',
-        ],
-        'brouchurelocal' => [
-            'database' => 'usvn',
-            'username' => 'sauem',
-            'password' => '123',
-        ]
-    ]
+    'db' => $local
 ];
