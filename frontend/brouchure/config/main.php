7f858d3c14acd71e28a343a6a16d7e5809479948
sauem <nguyendinhthang.go97@gmail.com>
2021-05-24 10:11:41 +0700

    brouchure

--- frontend/brouchure/config/main.php
+++ frontend/brouchure/config/main.php
@@ -3,7 +3,6 @@ require __DIR__ . '/const.php';
 $rules = require __DIR__ . '/rules.php';
 $params = array_merge(
     require __DIR__ . '/../../../common/config/params.php',
-    require __DIR__ . '/params-local.php',
     require __DIR__ . '/params.php'
 );
 
--- frontend/brouchure/web/index.php
+++ frontend/brouchure/web/index.php
@@ -1,7 +1,9 @@
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require __DIR__ . '/../../../env.php';
 require __DIR__ . '/../../../vendor/autoload.php';
 require __DIR__ . '/../../../vendor/yiisoft/yii2/Yii.php';
