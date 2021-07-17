607bda3aa36bbc5a4a81ecad8c48676efc817263
sauem <nguyendinhthang.go97@gmail.com>
2021-07-17 17:26:12 +0700

    d

--- frontend/brouchure/config/rules.php
+++ frontend/brouchure/config/rules.php
@@ -3,7 +3,7 @@ return [
     HOME => 'site/index',
     CONTACT => 'site/contact',
     // page
-    SERVICES => 'site/archive',
+    SERVICES => 'site/searvice',
     SERVICES . '/<archive:[0-9a-zA-Z\-]+>' => 'site/archive',
     ARTICLE . '/<archive:[0-9a-zA-Z\-]+>' => 'site/archive',
     ARTICLE . '/<archive:[0-9a-zA-Z\-]+>/<slug:[0-9a-zA-Z\-]+>' => 'site/detail-blog',
