fd67aabb5ebb0bb7427685c5ddd41777b66eefd6
sauem <nguyendinhthang.go97@gmail.com>
2021-08-17 17:39:55 +0700

    a

--- frontend/usvn/assets/AppAsset.php
+++ frontend/usvn/assets/AppAsset.php
@@ -27,7 +27,7 @@ class AppAsset extends AssetBundle
         '/css/color-default.css',
         '/css/retina.css',
         '/css/responsive.css',
-        '/css/custom.css?v=1.0'
+        '/css/custom.css?v=1.2'
     ];
     public $js = [
         '/js/jquery-2.1.4.min.js',
@@ -46,7 +46,7 @@ class AppAsset extends AssetBundle
         '/js/masterslider/masterslider.min.js',
         '/js/jquery.dlmenu.min.js',
         '/js/include.js',
-        '/js/site.js?v=1.9',
+        '/js/site.js?v=2.0',
     ];
     public $depends = [
         //'yii\web\YiiAsset',
