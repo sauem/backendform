5e905534da185e1ec90ea9ee2260b9a987bb0b1a
sauem <nguyendinhthang.go97@gmail.com>
2021-08-31 22:14:43 +0700

    a

--- common/messages/en-US/app-usvn.php
+++ common/messages/en-US/app-usvn.php
@@ -5,12 +5,14 @@ return [
     'rubber_price' => 'Rbber prices',
     'total_visited' => 'Total visited',
     'online' => 'Online',
-    'httpp'  => 'Distribution system and cooperation',
+    'httpp' => 'Distribution system and cooperation',
     'poll' => 'POLL',
     'result' => 'Result',
     'submit' => 'Submit',
     'library' => 'Libraries',
     'diff_product' => 'Other products',
     'form-intro' => 'Or contact us by filling out the following form:',
-    'form-required' => 'Required Information (*)'
+    'form-required' => 'Required Information (*)',
+    'cer' => 'Business registration certificate No. 0109231206 issued on June 18, 2020 by Business Registration Office – Hanoi Department of Planning and Investment.',
+    'policy' => 'Information privacy policy'
 ];
--- common/messages/vi-VN/app-usvn.php
+++ common/messages/vi-VN/app-usvn.php
@@ -12,5 +12,7 @@ return [
     'library' => 'Thư viện',
     'diff_product' => 'Sản phẩm khác',
     'form-intro' => 'Hoặc liên hệ với chúng tôi bằng cách điền thông tin vào form sau:',
-    'form-required' => 'Thông tin bắt buộc (*)'
+    'form-required' => 'Thông tin bắt buộc (*)',
+    'cer' => 'Giấy chứng nhận đăng ký kinh doanh số 0109231206 do Phòng Đăng ký kinh doanh - Sở Kế hoạch và Đầu tư thành phố Hà Nội cấp ngày 18/6/2020.',
+    'policy' => 'Chính sách bảo mật thông tin'
 ];
