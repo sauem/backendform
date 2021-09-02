d5cc67973951810019de24c7a85fd105f37e00ff
sauem <nguyendinhthang.go97@gmail.com>
2021-09-03 00:24:42 +0700

    a

--- frontend/usvn/controllers/SiteController.php
+++ frontend/usvn/controllers/SiteController.php
@@ -235,7 +235,7 @@ class SiteController extends BaseController
                     ->where(['default_archive' => $archive->id])
                     ->orWhere(['id' => $model->relations])
                     ->andFilterWhere(['!=', 'id', $model->id])
-                    ->limit(4)->all();
+                    ->limit(3)->all();
                 break;
             default:
                 $template = 'blog-detail.blade';
