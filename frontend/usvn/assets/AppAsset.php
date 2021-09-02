052afcd32ed5e10720dbc03378468c71ef3d5599
sauem <nguyendinhthang.go97@gmail.com>
2021-09-02 18:31:39 +0700

    a

--- frontend/usvn/assets/AppAsset.php
+++ frontend/usvn/assets/AppAsset.php
@@ -29,7 +29,7 @@ class AppAsset extends AssetBundle
         '/css/color-default.css',
         '/css/retina.css',
         '/css/responsive.css',
-        '/css/custom.css?v=4.5'
+        '/css/custom.css?v=4.6'
     ];
     public $js = [
         '/js/jquery-2.1.4.min.js',
