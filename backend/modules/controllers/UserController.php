20bd958f02ec6a48046a2b3efc510329297039b0
sauem <nguyendinhthang.go97@gmail.com>
2021-09-08 14:44:18 +0700

    a

--- backend/modules/controllers/UserController.php
+++ backend/modules/controllers/UserController.php
@@ -4,17 +4,41 @@
 namespace backend\modules\controllers;
 
 
+use common\helper\HelperFunction;
+use common\models\User;
 use common\models\UserModel;
 use common\models\UserSearch;
+use yii\helpers\ArrayHelper;
+use yii\web\BadRequestHttpException;
 
 class UserController extends BaseActiveFilterController
 {
     public $modelClass = UserModel::class;
+
     public function actions()
     {
         $actions = parent::actions();
+        unset($actions['update']);
         $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
         return $actions;
+    }
+
+    public function actionUpdate()
+    {
+        $model = UserModel::findOne(\Yii::$app->request->post('id'));
+        if (!$model) {
+            throw new BadRequestHttpException("User not found!");
+        }
+        $model->email = \Yii::$app->request->post('email');
+        $model->username = \Yii::$app->request->post('username');
+        $model->status = \Yii::$app->request->post('status');
+        if (\Yii::$app->request->post('new_password_hash')) {
+            $model->password_hash = $model->setPassword(\Yii::$app->request->post('new_password_hash'));
+        }
+        if (!$model->save()) {
+            throw new BadRequestHttpException(HelperFunction::firstError($model));
+        }
+        return $model;
     }
 
     public function prepareDataProvider($action, $filter)
