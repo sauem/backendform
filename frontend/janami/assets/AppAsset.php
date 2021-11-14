9708c246061fbba093739601eb3ab95b58e24f1c
sauem <nguyendinhthang.go97@gmail.com>
2021-11-14 23:26:04 +0700

    a

--- frontend/janami/assets/AppAsset.php
+++ frontend/janami/assets/AppAsset.php
@@ -36,7 +36,7 @@ class AppAsset extends AssetBundle
 //         '/css/silk-theme.css',
 //         '/css/gold-theme.css',
         '/css/responsive.css',
-        '/css/custom.css?v=5.2',
+        '/css/custom.css?v=5.3',
     ];
     public $js = [
         '/js/jquery-3.4.1.min.js',
