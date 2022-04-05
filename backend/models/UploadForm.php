2683dc8a0cc2e8d5e792f4357fbb72d95b4b7e92
sauem <nguyendinhthang.go97@gmail.com>
2022-04-05 21:39:21 +0700

    up

--- backend/models/UploadForm.php
+++ backend/models/UploadForm.php
@@ -61,7 +61,7 @@ class UploadForm extends Model
 
         if ($this->fileType !== self::TYPE_IMAGE_BASE64) {
             // $domain = HelperFunction::getDomain();
-            $basePath = '/';
+            $basePath = '/' . date("Ydm") . "/";
 
             $uploadFolder = UPLOAD_PATH . $basePath;
             if (!is_dir($uploadFolder)) {
