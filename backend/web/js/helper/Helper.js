41fe02d7327538f46a1c7b008989870cecb6099f
sauem <nguyendinhthang.go97@gmail.com>
2022-07-28 00:27:23 +0700

    uoasd

--- backend/web/js/helper/Helper.js
+++ backend/web/js/helper/Helper.js
@@ -108,7 +108,7 @@ function getParams(name) {
     return url.get(name);
 }
 
-function initTinymce(callback, editorClass = 'editor', defaultContent = null, height = 600) {
+function initTinymce(callback, editorClass = 'editor', defaultContent = 'Nội dung bài viết', height = 600) {
     // let useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
     tinymce.init({
@@ -118,8 +118,7 @@ function initTinymce(callback, editorClass = 'editor', defaultContent = null, he
             ' autolink autosave save directionality  visualblocks visualchars fullscreen' +
             ' image link media table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists  wordcount textpattern noneditable help charmap quickbars emoticons',
         images_upload_url: '/' + ROUTE.UPLOAD,
-        forced_root_block : "",
-        force_p_newlines : true,
+
         file_picker_callback: function (cb, value, meta) {
             let input = document.createElement('input');
             input.setAttribute('type', 'file');
